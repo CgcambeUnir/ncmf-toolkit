8bf2e631aedc0eb3d8d86b8bf58d3e90b5e1c53d
CgcambeUnir <carolina.gonzalez038@comunidadunir.net>
2026-07-27 18:47:46 +0200

    Initialize NCMF module with version and imports

---
+++ ncmf/_init_.py
@@ -0,0 +1,5 @@
+"""NCMF - Non-Compensatory Multimetric Framework for Technology Assessment."""
+from .core import (System, VetoChecklist, assess, tii, aif, ismi, cwri, rg_mpi,
+                   operational_maturity, dynamic_criticality)
+
+__version__ = "1.0.0"
